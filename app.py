ff62f32c49aa547c72a7ec899f51d180aca26d60
Ayhamhossam <ayham776457566@gmail.com>
2026-02-26 04:02:24 +0300

    Add app code

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+from flask import Flask, render_template, request, redirect, session
 
+app = Flask(__name__)
+app.secret_key = "marasim_secret_key"
+
+USERNAME = "ali"
+PASSWORD = "776940187"
+
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == USERNAME and password == PASSWORD:
+            session['user'] = username
+            return redirect('/dashboard')
+        else:
+            return "بيانات الدخول غير صحيحة"
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' not in session:
+        return redirect('/')
+    return render_template("dashboard.html")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run()
